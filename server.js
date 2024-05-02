7422f03083aaaa161b3b7303ed314c4bc3d69906
Taha Husnain <taha.husnain20@gmail.com>
2024-05-02 16:17:36 +0500

    pushed

---
+++ server.js
@@ -0,0 +1,21 @@
+import express from "express";
+import { Vonage } from "@vonage/server-sdk";
+
+const app = express();
+const port = 3000;
+app.use(express.json());
+
+const obj = {
+  name: "Taha",
+  Age: 23,
+  DOB: "2000-10-01",
+};
+
+// Example route to trigger OTP sending
+app.get("/", async (req, res) => {
+  res.send(obj);
+});
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`);
+});
